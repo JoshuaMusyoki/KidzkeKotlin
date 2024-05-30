066d36bb15f4daf25e9ada15b38da220cae36b98
Joshua_Musyoki <musyokijoxhua@gmail.com>
2024-05-30 19:34:06 +0300

    Setting up the Android Project Structure

---
+++ data/src/main/java/com/nerds/data/work/WorkConstants.kt
@@ -0,0 +1,6 @@
+package com.nerds.data.work
+
+object WorkConstants {
+    const val NOTIFICATIONCHANNEL = "notification_channel"
+    const val synchDataWorkerName ="sync_data"
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,30 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Kidzo24Kotlin"
+include(":app")
+include(":data")
+include(":local")
+include(":local")
+include(":remote")
+include(":presentation")
+include(":domain")
+include(":kidzke")
